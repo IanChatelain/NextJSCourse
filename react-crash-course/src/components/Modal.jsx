5203eea55209fbee1a75282abcbdf0fdab4b4e60
IanChatelain <ichatelain@academic.rrc.ca>
2024-05-09 12:59:48 -0500

    React router
    Next.js Course Section 2: Part 42

--- react-crash-course/src/components/Modal.jsx
+++ react-crash-course/src/components/Modal.jsx
@@ -1,12 +1,20 @@
+import { useNavigate } from "react-router-dom";
+
 import classes from "./Modal.module.css";
 
 // children prop can be drilled into without using props with {}.
 // children is a reserved prop.
 // open is a reserved prop as well and will evaluate to true by default.
-function Modal({ children, onClose }) {
+function Modal({ children }) {
+  const navigate = useNavigate();
+
+  function closeHanlder() {
+    navigate("..");
+  }
+
   return (
     <>
-      <div className={classes.backdrop} onClick={onClose}></div>
+      <div className={classes.backdrop} onClick={closeHanlder} />
       <dialog open className={classes.modal}>
         {children}
       </dialog>
