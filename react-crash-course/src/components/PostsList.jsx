3e32e8694d4c4c92f8d2df569a0989ba8bfd8324
IanChatelain <ichatelain@academic.rrc.ca>
2024-05-09 13:30:35 -0500

    React loader
    Implemented loaded instead of useEffect(). *NOTE* After the course, look up how to use the loader to load specific content instead of loading nothing until it is complete

--- react-crash-course/src/components/PostsList.jsx
+++ react-crash-course/src/components/PostsList.jsx
@@ -1,23 +1,10 @@
-import { useState, useEffect } from "react";
+import { useLoaderData } from "react-router-dom";
 
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 
 function PostsList() {
-  const [posts, setPosts] = useState([]);
-  const [isFetching, setIsFetching] = useState(false);
-
-  useEffect(() => {
-    async function fetchPosts() {
-      setIsFetching(true);
-      const response = await fetch("http://localhost:8080/posts");
-      const resData = await response.json();
-      setPosts(resData.posts);
-      setIsFetching(false);
-    }
-
-    fetchPosts();
-  }, []);
+  const posts = useLoaderData();
 
   function addPostHandler(postData) {
     fetch("http://localhost:8080/posts", {
@@ -35,7 +22,7 @@ function PostsList() {
 
   return (
     <>
-      {!isFetching && posts.length > 0 && (
+      {posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
             // post.body could be post.id instead, unique ID for the array element
@@ -43,15 +30,10 @@ function PostsList() {
           ))}
         </ul>
       )}
-      {!isFetching && posts.length === 0 && (
+      {posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
           <h2>There are no posts yet</h2>
           <p>Start adding some posts!</p>
-        </div>
-      )}
-      {isFetching && (
-        <div style={{ textAlign: "center", color: "white" }}>
-          <p>Loading posts...</p>
         </div>
       )}
     </>
