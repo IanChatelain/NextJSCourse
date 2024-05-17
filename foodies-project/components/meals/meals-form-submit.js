dc330baae3a4275a7ac40975bcfcc4e2d283ca4d
IanChatelain <ichatelain@academic.rrc.ca>
2024-05-17 09:32:15 -0500

    useFormStatus issue RESOLVED
    line 9: | instead of :

--- foodies-project/components/meals/meals-form-submit.js
+++ foodies-project/components/meals/meals-form-submit.js
@@ -5,7 +5,9 @@ import { useFormStatus } from "react-dom";
 export default function MealsFormSubmit() {
   const { pending } = useFormStatus();
 
-  return <button disabled={pending}>
-    {pending ? 'Submitting..' | 'Share Meal'}
-  </button>
+  return (
+    <button disabled={pending}>
+      {pending ? "Submitting.." : "Share Meal"}
+    </button>
+  );
 }
